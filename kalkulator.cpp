d4d45472e5eed8b27a2e03252c1aeeb7187b0a2e
Fahrulz <fahrulezhi@gmail.com>
2022-11-08 17:00:10 +0700

    adding kalkulator code

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,20 +1,46 @@
-// kalkulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <conio.h>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int a, b, pilihan;
+	string operasi;
+	double hasil;
+
+	cout << "Welcome to aritmatika kalkulator by fahrul" << endl;
+	cout << "\n";
+
+	cout << "1. Penjumlahan" << endl;
+	cout << "2. Pengurangan" << endl;
+	cout << "3. Perkalian" << endl;
+
+	cout << "Silahakan pilih nomor yang ada di atas: ";
+	cin >> pilihan;
+
+	cout << "Enter first value: ";
+	cin >> a;
+
+	cout << "Enter second value: ";
+	cin >> b;
+
+	switch (pilihan) {
+	case 1:
+		hasil = a + b;
+		operasi = '+';
+		break;
+	case 2:
+		hasil = a - b;
+		operasi = '-';
+		break;
+	case 3:
+		hasil = a * b;
+		operasi = '*';
+		break;
+	default:
+		cout << "Pilihan nomor yang anda pilih tidak terdaftar!" << endl;
+	}
+
+	cout << "------------------------------" << endl;
+	cout << a << operasi << b << " = " << hasil << endl;
+	cout << "------------------------------" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
